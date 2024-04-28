411c149edc9051ea5b5b9562cc0902a9a9102acf
homi <entekk1112@naver.com>
2024-04-28 17:18:18 +0900

    useReducer

--- src/pages/todo/Todo.js
+++ src/pages/todo/Todo.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
 import TodoHd from './components/TodoHd';
 import TodoEditor from './components/TodoEditor';
 import TodoList from './components/TodoList';
@@ -24,45 +24,79 @@ const mockTodo = [
   },
 ]
 
-const Todo = () => {
-  const [todo, setTodo] = useState(mockTodo)
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD':
+      return [action.payload, ...state]
+    case 'update':
+      return state.map((item) => (item.id === action.payload ? {...item, isDone: !item.isDone} : item))
+    case 'delete':
+      return state.filter((item) => item.id !== action.payload)
+    default:
+      return state
+  }
+}
 
-  //todo 추가
+const Todo = () => {
+  // const [todo, setTodo] = useState(mockTodo)
+
+  // //todo 추가
+  // const addTodo = (task) => {
+  //   const newTodo = {
+  //     id : todo.length + 1,
+  //     isDone : false,
+  //     task,
+  //     createdDate : new Date().getTime()
+  //   }
+  //   setTodo([newTodo, ...todo]) //새로운 todo를 맨 앞쪽에 추가
+  // }
+
+  // //todo 업데이트
+  // const updateTodo = (id) => {
+  //   setTodo(
+  //     todo.map((item) => {
+  //       if (item.id === id) {
+  //         return{
+  //           ...item,
+  //           isDone:!item.isDone,
+  //         }
+  //       }
+  //       return item
+  //     })
+  //   )
+  // }
+
+  // //todo 삭제
+  // const deleteTodo = (id) => {
+  //   setTodo(todo.filter((item) => item.id !== id))
+  // }
+
+  const [state, dispatch] = useReducer(reducer, mockTodo)
+
   const addTodo = (task) => {
     const newTodo = {
-      id : todo.length + 1,
+      id : state.length + 1,
       isDone : false,
       task,
       createdDate : new Date().getTime()
     }
-    setTodo([newTodo, ...todo]) //새로운 todo를 맨 앞쪽에 추가
+    dispatch({type: 'ADD', payload: newTodo}) //새로운 todo를 맨 앞쪽에 추가
   }
 
-  //todo 업데이트
   const updateTodo = (id) => {
-    setTodo(
-      todo.map((item) => {
-        if (item.id === id) {
-          return{
-            ...item,
-            isDone:!item.isDone,
-          }
-        }
-        return item
-      })
-    )
+    dispatch({type:'update', payload: id})
   }
 
-  //todo 삭제
   const deleteTodo = (id) => {
-    setTodo(todo.filter((item) => item.id !== id))
+    dispatch({type:'delete', payload: id})
   }
+
 
   return (
     <div>
       <TodoHd />
       <TodoEditor addTodo={addTodo}/>
-      <TodoList todo={todo} updateTodo={updateTodo} deleteTodo={deleteTodo}/>
+      <TodoList todo={state} updateTodo={updateTodo} deleteTodo={deleteTodo}/>
     </div>
 
   )
